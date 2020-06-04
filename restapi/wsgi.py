1cc1d833c11467eecf6298d547a668a9636425f3
Yannick Ulmrich <yannickulmrich@gmail.com>
2020-06-04 20:17:03 +0200

    changed app to application

--- restapi/wsgi.py
+++ restapi/wsgi.py
@@ -4,10 +4,10 @@ print("This is the wsgi.py file")
 
 def create_app(testing: bool = True):
     print("This is the create_app method")
-    app = Flask(__name__)
+    application = Flask(__name__)
 
-    @app.route("/")
+    @application.route("/")
     def index():
         return f"Hello World!<br>Testing: {testing}"
 
-    return app
+    return application
