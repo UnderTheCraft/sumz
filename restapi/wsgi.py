ca38a12e0fe2cc246c04ba81fdbe08e14453618e
Yannick Ulmrich <yannickulmrich@gmail.com>
2020-06-04 20:22:38 +0200

    refactored wsgi.py

--- restapi/wsgi.py
+++ restapi/wsgi.py
@@ -2,12 +2,18 @@ from flask import Flask
 
 print("This is the wsgi.py file")
 
+
 def create_app(testing: bool = True):
     print("This is the create_app method")
-    application = Flask(__name__)
+    app = Flask(__name__)
 
-    @application.route("/")
+    @app.route("/")
     def index():
         return f"Hello World!<br>Testing: {testing}"
 
-    return application
+    return app
+
+
+application = create_app(testing = True)
+
+print("The application object is now created!")
