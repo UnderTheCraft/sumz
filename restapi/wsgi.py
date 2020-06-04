b43b9799ed3dbdd96bccaa1153107ea46a24e77b
Yannick Ulmrich <yannickulmrich@gmail.com>
2020-06-04 20:55:11 +0200

    added echo and company

--- restapi/wsgi.py
+++ restapi/wsgi.py
@@ -1,19 +1,41 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
+from flask_api import status
 
-print("This is the wsgi.py file")
-
-
-def create_app(testing: bool = True):
-    print("This is the create_app method")
-    app = Flask(__name__)
-
-    @app.route("/")
-    def index():
-        return f"Hello World!<br>Testing: {testing}"
-
-    return app
+application = Flask(__name__)
 
 
-application = create_app(testing = True)
+@application.route("/")
+def index():
+    return "This is the backend of sumz!"
 
-print("The application object is now created!")
+
+@application.route("/echo/<string:message>")
+def index(message):
+    return f"This is your echo\n {message}"
+
+
+@application.route("/getCashFlows/<company>")
+def index(company):
+    if "adidas".__eq__(company.casefold()):
+        response = jsonify(
+            company=company,
+            startingDate="01.01.2008",
+            freeCashFlows=[22,55,64,97,87,155,99,124,136]
+        )
+    elif "allianz".__eq__(company.casefold()):
+        response = jsonify(
+            company=company,
+            startingDate="01.01.2012",
+            freeCashFlows=[42, 75, 61, 87, 97, 125, 110, 124, 136]
+        )
+    elif "sap".__eq__(company.casefold()):
+        response = jsonify(
+            company=company,
+            startingDate="01.01.2014",
+            freeCashFlows=[24, 68, 100]
+        )
+    # TODO: Add companies
+    else:
+        return make_response(jsonify(), status.HTTP_404_NOT_FOUND)
+
+    return make_response(response, status.HTTP_200_OK)
