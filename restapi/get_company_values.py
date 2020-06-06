579d9e939ebc58d5df0a9e0255a8cd10440a6a3a
Christian Gunzelmann D070429 <christian.gunzelmann@sap.com>
2020-06-06 18:30:13 +0200

    Code to get FCFs from yCharts

--- restapi/get_company_values.py
+++ restapi/get_company_values.py
@@ -1,4 +1,6 @@
+from requests_html import HTMLSession
 import pandas as pd
+from dateutil.parser import parse
 
 # get cash flows of companies
 def get_cash_flows(company: str):
@@ -15,10 +17,24 @@ def get_cash_flows(company: str):
         print(e)
         print("company not found locally")
 
+
     # Get from an API
     try:
-      # TODO request api or
-      print("todo")
+        abbrevationToNumber = {'K': 3, 'M': 6, 'B': 9, 'T': 12}
+        session = HTMLSession()
+        response = session.get('https://ycharts.com/companies/' + company + '/free_cash_flow')
+        rawDates = response.html.find(".histDataTable", first=True).find(".col1")
+        rawFCFs = response.html.find(".histDataTable", first=True).find(".col2")
+
+        FCFs = []
+
+        for i in range(1, len(rawDates)):
+            parsedDate = parse(rawDates[i].text)
+            parsedFCF = int(float(rawFCFs[i].text[:-1]) * 10 ** abbrevationToNumber[rawFCFs[i].text[-1]])
+
+            FCFs.append([parsedDate, parsedFCF]).to_json(orient='records')
+
+        return pd.DataFrame(FCFs[0:16], columns=['Date', 'FCF'])
     except Exception:
         print("company not available within API")
 
