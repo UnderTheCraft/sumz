dd91410c9175bd39b7cd89a39e341d81ae322fa2
Yannick Ulmrich <yannickulmrich@gmail.com>
2020-06-07 16:17:50 +0200

    refactored get_company_values

--- restapi/get_company_values.py
+++ restapi/get_company_values.py
@@ -1,32 +1,55 @@
 from requests_html import HTMLSession
 import pandas as pd
 import json
+import traceback
+
 from dateutil.parser import parse
 
 # get cash flows of companies
 def get_cash_flows(company: str):
 
-    base_dir = "https://cloud-cube-eu.s3.amazonaws.com/mm6r5v7viahe/public"
+    local_companies = ["sap"]
+    api_companies = ["AAPL", "MSFT"]
 
-    # Get from local files
+    if company.casefold() in local_companies:
+        return get_from_local(company)
+    elif company in api_companies:
+        return get_from_api(company)
+    else:
+        try:
+            cash_flows = get_from_api(company)
+            return cash_flows
+        except Exception as e:
+            traceback.print_exc()
+            raise NotImplementedError(f"Company {company} not available locally and within API")
+
+# Get from local files
+def get_from_local(company: str):
+    base_dir = "https://cloud-cube-eu.s3.amazonaws.com/mm6r5v7viahe/public"
 
     try:
         path = f"{base_dir}/{company.casefold()}.csv"
         #print(path)
-        df = pd.read_csv(path)
-        print("DataFrame successfully read from S3 bucket -> Return values")
-        return json.loads(df.to_json(orient='records'))
+        result_df = pd.read_csv(path)
+        #print("DataFrame successfully read from S3 bucket -> Return values")
+        return json.loads(result_df.to_json(orient='records'))
         #return df.to_json(orient='records')
     except FileNotFoundError as e:
         print("company not found locally")
-        #print(e)
+        traceback.print_exc()
 
-
-    # Get from an API
+# Get from an API
+def get_from_api(company: str):
     try:
+        print("get from API")
         abbrevationToNumber = {'K': 3, 'M': 6, 'B': 9, 'T': 12}
         session = HTMLSession()
+
+        # TODO hier sollte vielleicht ne try hin, falls keine Verbindung aufgebaut werden kann
         response = session.get('https://ycharts.com/companies/' + company + '/free_cash_flow')
+
+        print(f"The headers of the requests are:\n{response.headers}")
+
         rawDates = response.html.find(".histDataTable", first=True).find(".col1")
         rawFCFs = response.html.find(".histDataTable", first=True).find(".col2")
 
@@ -38,11 +61,15 @@ def get_cash_flows(company: str):
 
             FCFs.append([parsedDate, parsedFCF])
 
-        return pd.DataFrame(FCFs[0:16], columns=['Date', 'FCF']).to_json(orient='records')
-    except Exception:
-        print("company not available within API")
+        result_df = pd.DataFrame(FCFs[0:16], columns=['Date', 'FCF'])
+        return json.loads(result_df.to_json(orient='records'))
 
-    raise NotImplementedError(f"Company {company} not available locally and within API")
+    except Exception as e:
+        print(f"company not available within API!")
+        traceback.print_exc()
+
+# ---------------------------------------------------------------------------------------------------------------------
+
 
 # get beta factor
 
