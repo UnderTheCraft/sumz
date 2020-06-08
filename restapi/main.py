8fe41af2ebc0c1999f87ba80a2c8cadf9a7f6e85
Yannick Ulmrich <yannickulmrich@gmail.com>
2020-06-08 16:16:20 +0200

    removed methods

--- restapi/main.py
+++ restapi/main.py
@@ -32,37 +32,37 @@ class Companies(Resource):
         return companyInfo.get_all_companies()
 
 
-@application.route("/methods")
-class Methods(Resource):
-    def get(self):
-        return {"methods": {"name": "APV", "description": "Adjusted Present Value"}}
-
-
-@application.route("/getCashFlows/<string:company>")
-class CashFlows(Resource):
-    def get(self, company):
-        try:
-            company_cash_flows = companyValues.get_cash_flows(company)
-            company_cash_flows.append({"company": company.casefold()})
-
-            # cf_response = Response(json.dump(company_cash_flows))
-            # print("created cf_response object")
-
-            response = make_response(jsonify(company_cash_flows), status.HTTP_200_OK)
-            response.headers['content-type'] = 'application/json'
-            print("returning response object")
-            return response
-
-        except NotImplementedError as e:
-            return make_response(f"Das Unternehmen {company} ist nicht verfügbar {str(e)}", status.HTTP_404_NOT_FOUND)
-        except Exception as e:
-            print("Es ist ein schwerwiegender Fehler aufgetreten")
-            print(e)
-
-        return make_response(f"Die Anfrage für das Unternehmen {company} konnte nicht bearbeitet werden!")
-
-
-@application.route("/getCashFlowForecast/<string:company>&prediction_length=<int:prediction_length>")
-class CashFlowForecast(Resource):
-    def get(self, company, prediction_length):
-        return make_response(f"Company {company} and prediction length {prediction_length}", status.HTTP_200_OK)
+# @application.route("/methods")
+# class Methods(Resource):
+#     def get(self):
+#         return {"methods": {"name": "APV", "description": "Adjusted Present Value"}}
+#
+#
+# @application.route("/getCashFlows/<string:company>")
+# class CashFlows(Resource):
+#     def get(self, company):
+#         try:
+#             company_cash_flows = companyValues.get_cash_flows(company)
+#             company_cash_flows.append({"company": company.casefold()})
+#
+#             # cf_response = Response(json.dump(company_cash_flows))
+#             # print("created cf_response object")
+#
+#             response = make_response(jsonify(company_cash_flows), status.HTTP_200_OK)
+#             response.headers['content-type'] = 'application/json'
+#             print("returning response object")
+#             return response
+#
+#         except NotImplementedError as e:
+#             return make_response(f"Das Unternehmen {company} ist nicht verfügbar {str(e)}", status.HTTP_404_NOT_FOUND)
+#         except Exception as e:
+#             print("Es ist ein schwerwiegender Fehler aufgetreten")
+#             print(e)
+#
+#         return make_response(f"Die Anfrage für das Unternehmen {company} konnte nicht bearbeitet werden!")
+#
+#
+# @application.route("/getCashFlowForecast/<string:company>&prediction_length=<int:prediction_length>")
+# class CashFlowForecast(Resource):
+#     def get(self, company, prediction_length):
+#         return make_response(f"Company {company} and prediction length {prediction_length}", status.HTTP_200_OK)
