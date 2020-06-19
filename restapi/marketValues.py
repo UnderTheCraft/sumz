80a163eaa9e71424aaff42b47d16f5ef43e2e8d8
Chr1st1an02 <guch99@gmx.de>
2020-06-19 11:41:36 +0200

    Bug Fix

--- restapi/marketValues.py
+++ restapi/marketValues.py
@@ -1,18 +1,18 @@
 class MarketValues:
 
-    def __init__(self):
-        """ Risikoloser Zins:
-        Kann anhand der Rendite von Staatsanleihen (30 Jahre) festgelegt werden.
-        In diesem Fall sind dies US-Staatsanleihen, siehe https://www.investing.com/rates-bonds/u.s.-30-year-bond-yield
-        Stand 06/2020: Risikoloser Zins standardmäßig auf 1.5 """
-        self.__risk_free_interest = 1.5
 
-        """ Marktrisikoprämie:
-        Langfristige Entwicklung des Marktes - dem Risikolosen Zins.
-        In diesem Fall hat der Dow Jones über 30 Jahre hinweg einen jährlichen Zuwachs von 9 %. """
-        self.__market_risk_premium = 7.5
+    """ Risikoloser Zins:
+    Kann anhand der Rendite von Staatsanleihen (30 Jahre) festgelegt werden.
+    In diesem Fall sind dies US-Staatsanleihen, siehe https://www.investing.com/rates-bonds/u.s.-30-year-bond-yield
+    Stand 06/2020: Risikoloser Zins standardmäßig auf 1.5 """
+    __default__risk_free_interest = 1.5
 
-    def __init__(self, risk_free_interest: float, market_risk_premium: float):
+    """ Marktrisikoprämie:
+    Langfristige Entwicklung des Marktes - dem Risikolosen Zins.
+    In diesem Fall hat der Dow Jones über 30 Jahre hinweg einen jährlichen Zuwachs von 9 %. """
+    __default__market_risk_premium = 7.5
+
+    def __init__(self, risk_free_interest: float = __default__risk_free_interest, market_risk_premium: float = __default__market_risk_premium):
         self.__risk_free_interest = risk_free_interest
         self.__market_risk_premium = market_risk_premium
 
