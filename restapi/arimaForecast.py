1bf7d55ef3c89e498ec4572ee280648ec3cb55df
Chr1st1an02 <guch99@gmx.de>
2020-06-20 14:12:18 +0200

    BUG FIX: Added logic to APV class and URL for APV method

--- restapi/arimaForecast.py
+++ restapi/arimaForecast.py
@@ -9,7 +9,7 @@ class ARIMAForecast:
     def __init__(self):
         print('ARIMA Forecast Object created')
 
-    def make_forecast(df: pd.DataFrame, forecast_length: int):
+    def make_forecast(self, df: pd.DataFrame, forecast_length: int):
         reversed(df)
         model = SARIMAX(df, order=(1, 0, 0), seasonal_order=(0, 1, 1, 4), seasonal=True)
         trained_model = model.fit()
