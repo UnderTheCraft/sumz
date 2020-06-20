03b4692f219442cfbbfcb3b907b0202d81156149
Chr1st1an02 <guch99@gmx.de>
2020-06-20 14:33:50 +0200

    APV correction and prints for debug

--- restapi/APV.py
+++ restapi/APV.py
@@ -41,17 +41,17 @@ class APV(BaseMethod):
 
         self.number_of_values_for_forecast = len(past_fcfs)
 
-        print("Past fcfs "+past_fcfs)
+        print("Past fcfs "+str(past_fcfs))
 
         forecast_fcfs_quarterly = ARIMAForecast().make_forecast(past_fcfs, 20)
         print("FCF quarterly "+forecast_fcfs_quarterly)
 
         forecast_fcfs_year = np.sum(np.array_split(forecast_fcfs_quarterly, 4), axis=1)
-        print("FCF year" + forecast_fcfs_year)
+        print("FCF year" + str(forecast_fcfs_year))
 
         GKu = 0
         equity_interest = self.calculateEquityInterest()/100
-        print("Equityinterest " + equity_interest)
+        print("Equityinterest " + str(equity_interest))
 
         for i in range(len(forecast_fcfs_year) - 1):
             GKu = GKu + (forecast_fcfs_year / ((1 + equity_interest) ** i))
