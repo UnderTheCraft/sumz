bfbc79f4278c3f2e6b50cdec059adbdec75e5be2
Chr1st1an02 <guch99@gmx.de>
2020-06-23 12:42:23 +0200

    Correction: Arima forecast

--- restapi/arimaForecast.py
+++ restapi/arimaForecast.py
@@ -10,7 +10,6 @@ class ARIMAForecast:
         print('ARIMA Forecast Object created')
 
     def make_forecast(self, df: pd.DataFrame, forecast_length: int):
-        reversed(df)
         model = SARIMAX(df, order=(1, 0, 0), seasonal_order=(0, 1, 1, 4), seasonal=True)
         trained_model = model.fit()
         print(trained_model.summary())
