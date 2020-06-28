23a9281fcb366ace52109cbcb06383f6161360e8
Chr1st1an02 <guch99@gmx.de>
2020-06-28 14:13:31 +0200

    Refactor baseInforamtion.py

--- restapi/APVInformation.py
+++ restapi/APVInformation.py
@@ -5,4 +5,4 @@ from restapi.baseInforamtion import BaseInformation
 class APVInformation(BaseInformation):
 
     def __init__(self):
-        super().__init__(method_name="APV", method_description="Adjusted Present Value", methodClass=APV)
+        super().__init__(method_name="APV", method_description="Adjusted Present Value", method_class=APV)
--- restapi/FCFInformation.py
+++ restapi/FCFInformation.py
@@ -5,4 +5,4 @@ from restapi.baseInforamtion import BaseInformation
 class FCFInformation(BaseInformation):
 
     def __init__(self):
-        super().__init__(method_name="FCF", method_description="Free Cash Flow Method", methodClass=FCF)
+        super().__init__(method_name="FCF", method_description="Free Cash Flow Method", method_class=FCF)
