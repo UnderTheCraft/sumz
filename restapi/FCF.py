138b532e8f28321560783ed37f7d890d6af8c555
Chr1st1an02 <guch99@gmx.de>
2020-06-28 15:38:46 +0200

    FCF Method

--- restapi/FCF.py
+++ restapi/FCF.py
@@ -1,7 +1,14 @@
 from restapi.baseMethod import BaseMethod
-
+from math import floor
 
 class FCF(BaseMethod):
 
     def __init__(self):
         pass
+
+    def calculateEnterpriseValue(self):
+        """ Hauptmethode für die Berechnung des Unternehmenswertes """
+
+        enterprise_value = self.calculatePresentValueOfCashFlow() - self.getDebt()
+
+        return floor(enterprise_value)
