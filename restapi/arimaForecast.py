f70e08f4b73db7cca03d21840d7a01c9c598e548
Chr1st1an02 <guch99@gmx.de>
2020-07-01 15:34:37 +0200

    ARIMA TEST: enforce_stationarity=False, enforce_invertibility=False

--- restapi/arimaForecast.py
+++ restapi/arimaForecast.py
@@ -9,7 +9,7 @@ class ARIMAForecast:
         print('ARIMA Forecast Object created')
 
     def make_forecast(self, df: pd.DataFrame, forecast_length: int):
-        model = SARIMAX(df, order=(1, 0, 0), seasonal_order=(0, 1, 1, 4), enforce_stationarity=True, enforce_invertibility=False)
+        model = SARIMAX(df, order=(1, 0, 0), seasonal_order=(0, 1, 1, 4), enforce_stationarity=False, enforce_invertibility=False)
         trained_model = model.fit()
         print(trained_model.summary())
         forecast = trained_model.forecast(forecast_length)
